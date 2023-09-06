f015bd6835ffe911405e01834be65d9a2e02b594
Christopher Cottonaro <108497027+Barti9871@users.noreply.github.com>
2023-09-05 19:03:33 -0500

    content

--- coinmain/src/App.js
+++ coinmain/src/App.js
@@ -1,10 +1,20 @@
-
+import Home from "./pages/Home";
+import "../src/dist/styles.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Coin from "./pages/Coin";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />}></Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
